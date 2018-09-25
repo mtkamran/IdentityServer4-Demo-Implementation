2936b6b245b6a90f0bff07c5b1e072368a2103a1
mtkamran <mtkamran@naeir.org>
2018-09-25 13:24:45 -0500

    Initial Commit of IdenityServer4 Setup

---
+++ 02/demos/start-end/src/ImageGallery.Client/Startup.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using ImageGallery.Client.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using IdentityModel;
+using System.IdentityModel.Tokens.Jwt;
+
+
+
+namespace ImageGallery.Client
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+
+            // section 5 demo 1
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        //public Startup(IConfiguration configuration)
+        //{
+        //    //Configuration = configuration;
+        //    JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+        //}
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            // register an IHttpContextAccessor so we can access the current
+            // HttpContext in services by injecting it
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // register an IImageGalleryHttpClient
+            services.AddScoped<IImageGalleryHttpClient, ImageGalleryHttpClient>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+
+            }).AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options => 
+            {
+                options.SignInScheme = "Cookies";
+                options.Authority = "https://localhost:44369/";
+                options.ClientId = "imagegalleryclient";
+                options.ResponseType = "code id_token";
+                //options.CallbackPath = new PathString("...");
+                //options.SignedOutCallbackPath = new PathString("...");
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("address");
+                options.SaveTokens = true;
+                options.ClientSecret = "secret";
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.ClaimActions.Remove("amr");
+                options.ClaimActions.DeleteClaim("sid");
+                options.ClaimActions.DeleteClaim("idp");
+                //options.ClaimActions.DeleteClaim("address");
+            }); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else 
+            {
+                app.UseExceptionHandler("/Shared/Error");
+            }
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Gallery}/{action=Index}/{id?}");
+            });
+        }         
+    }
+}
